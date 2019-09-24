a2e2ed5f4475a8c00b46616c0c4cfaff5719721f
aaron <alkrauss48@gmail.com>
2019-09-24 13:41:52 -0500

    Add .editorconfig Update FeedbackForm vue component

--- src/js/FeedbackForm.vue
+++ src/js/FeedbackForm.vue
@@ -107,7 +107,6 @@
 </style>
 
 <script>
-import Vue from 'vue'
 import axios from 'axios';
 
 // Closable Directive courtesy of
@@ -118,49 +117,49 @@ import axios from 'axios';
 let handleOutsideClick;
 
 const Closable = {
-  bind (el, binding, vnode) {
-    // Here's the click/touchstart handler
-    // (it is registered below)
-    handleOutsideClick = (e) => {
-      e.stopPropagation();
-      // Get the handler method name and the exclude array
-      // from the object used in v-closable
-      const { handler, exclude } = binding.value;
+    bind(el, binding, vnode) {
+        // Here's the click/touchstart handler
+        // (it is registered below)
+        handleOutsideClick = (e) => {
+            e.stopPropagation();
+            // Get the handler method name and the exclude array
+            // from the object used in v-closable
+            const { handler, exclude } = binding.value;
 
-      // This variable indicates if the clicked element is excluded
-      let clickedOnExcludedEl = false;
-      exclude.forEach(refName => {
-        // We only run this code if we haven't detected
-        // any excluded element yet
-        if (!clickedOnExcludedEl) {
-          // Get the element using the reference name
-          const excludedEl = vnode.context.$refs[refName];
-          // See if this excluded element
-          // is the same element the user just clicked on
-          clickedOnExcludedEl = excludedEl.contains(e.target);
-        }
-      });
+            // This variable indicates if the clicked element is excluded
+            let clickedOnExcludedEl = false;
+            exclude.forEach((refName) => {
+                // We only run this code if we haven't detected
+                // any excluded element yet
+                if (!clickedOnExcludedEl) {
+                    // Get the element using the reference name
+                    const excludedEl = vnode.context.$refs[refName];
+                    // See if this excluded element
+                    // is the same element the user just clicked on
+                    clickedOnExcludedEl = excludedEl.contains(e.target);
+                }
+            });
 
-      // We check to see if the clicked element is not
-      // the dialog element and not excluded
-      if (!el.contains(e.target) && !clickedOnExcludedEl) {
-        // If the clicked element is outside the dialog
-        // and not the button, then call the outside-click handler
-        // from the same component this directive is used in
-        vnode.context[handler]();
-      }
-    }
-    // Register click/touchstart event listeners on the whole page
-    document.addEventListener('click', handleOutsideClick);
-    document.addEventListener('touchstart', handleOutsideClick);
-  },
+            // We check to see if the clicked element is not
+            // the dialog element and not excluded
+            if (!el.contains(e.target) && !clickedOnExcludedEl) {
+                // If the clicked element is outside the dialog
+                // and not the button, then call the outside-click handler
+                // from the same component this directive is used in
+                vnode.context[handler]();
+            }
+        };
+        // Register click/touchstart event listeners on the whole page
+        document.addEventListener('click', handleOutsideClick);
+        document.addEventListener('touchstart', handleOutsideClick);
+    },
 
-  unbind () {
-    // If the element that has v-closable is removed, then
-    // unbind click/touchstart listeners from the whole page
-    document.removeEventListener('click', handleOutsideClick);
-    document.removeEventListener('touchstart', handleOutsideClick);
-  }
+    unbind() {
+        // If the element that has v-closable is removed, then
+        // unbind click/touchstart listeners from the whole page
+        document.removeEventListener('click', handleOutsideClick);
+        document.removeEventListener('touchstart', handleOutsideClick);
+    },
 };
 
 export default {
@@ -179,7 +178,7 @@ export default {
     },
 
     directives: {
-      Closable,
+        Closable,
     },
 
     computed: {
@@ -205,8 +204,8 @@ export default {
             this.isActive = !this.isActive;
         },
 
-        onClose () {
-          this.isActive = false;
+        onClose() {
+            this.isActive = false;
         },
 
         async onSubmit() {
@@ -233,7 +232,7 @@ export default {
 
         reset() {
             this.body = null;
-        }
+        },
     },
 
     mounted() {
@@ -241,15 +240,15 @@ export default {
     },
 
     watch: {
-      isActive(value) {
-        if (value) {
-          return;
-        }
+        isActive(value) {
+            if (value) {
+                return;
+            }
 
-        // Reset the state of the form.
-        this.success = false;
-        this.errors = null;
-      },
+            // Reset the state of the form.
+            this.success = false;
+            this.errors = null;
+        },
     },
 };
 </script>
